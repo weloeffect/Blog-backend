6015465a14dec46e8ba9661e4c59b5bb79c7e29b
Walid Mohamed <walidridge1@gmail.com>
2022-09-27 14:27:44 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/images', express.static('images')); 
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+
+const connection = mongoose.connection;
+connection.once('open', () =>{
+    console.log("Mongodb database connection established successfully");
+})
+
+const userRouter =  require('./routes/user.route')
+const adminRouter =  require('./routes/admin/admin.route')
+const postRouter =  require('./routes/post.route')
+const catRouter =  require('./routes/category.route')
+// const categoryRouter =  require('./routes/category.route')
+app.use('/user', userRouter)
+app.use('/admin', adminRouter)
+app.use('/posts', postRouter)
+app.use('/categories', catRouter)
+// app.use('/categories', categoryRouter)
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+})
